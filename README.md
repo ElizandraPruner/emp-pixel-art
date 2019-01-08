6373732e02a213f53011dada8db3f477496e01b2
Elizandra Pruner <elizandramachado87@hotmail.com>
2019-01-07 22:41:01 -0200

    commit pixel art

---
+++ README.md
@@ -0,0 +1,25 @@
+# Pixel Art Maker Project
+
+## Table of Contents
+
+* [Instructions](#instructions)
+* [Contributing](#contributing)
+
+## Instructions
+
+To get started, open `designs.js` and start building out the app's functionality.
+
+For specific, detailed instructions, look at the project instructions in the [Udacity Classroom](https://classroom.udacity.com/me).
+
+## Contributing
+
+This repository is the starter code for _all_ Udacity students. Therefore, we most likely will not accept pull requests.
+
+Instructions:
+
+Insert the number of columns and the number of lines you want to draw and press Draw Grid, then choose the color on the color picker and have fun.
+
+How to acess the game:
+
+This game is hosted on https://elizandrapruner.github.io/emp-pixel-art
+
---
+++ designs.js
@@ -0,0 +1,27 @@
+// Your code goes here!
+$("button[type='submit']").on('click', function(event){
+	
+	event.preventDefault();
+	
+	const height = $('#inputHeight').val(); // Select size input
+	const width = $('#inputWidth').val(); // Select size input
+	
+	$('#pixelCanvas').empty(); //clean the grid to start all over again
+
+	makeGrid(height, width); // When size is submitted by the user, call makeGrid()
+});
+/*function to draw the grid*/
+function makeGrid(height, width) {
+	for(i = 0; i < height; i++){
+		$("#pixelCanvas").append($("<tr></tr>"));
+		for(j = 0; j < width; j++){
+			$("tr").last().append($("<td></td>"));
+		}	
+	}
+
+	$("td").on('click', function(event){
+		const painting = $('#colorPicker').val(); // Select color input
+		$(event.target).css('background-color', painting); //painting the background of td with color picked by user
+	});
+
+}
